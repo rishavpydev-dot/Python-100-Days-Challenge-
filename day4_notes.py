0934faf226f93b85f289da81de840ffc90085a73
rishavpydev-dot <rishavpy.dev@gmail.com>
2025-12-30 19:46:07 +0530

    Add day4_notes.py with Python basics
    This file contains basic examples of Python print statements and arithmetic operations.

---
+++ day4_notes.py
@@ -0,0 +1,28 @@
+#first python program 
+#print is a function which is used to perform any task 
+#kisi bhi funtion ko invoke karne ke liye() use karte hain 
+#ex:-
+print('Hello World')
+print(7)
+print("rahul")
+print('7')
+#aur agar ('') ya ("") lagakar kuch bhi lekhe to wo string hoga aur agar bina string lagaye lekhe number to wo number hoga par agar kisi letter ko lekhe bina quotes ke to wo snatax error aayega aur agar quotes ke sath lekhe to string for ex:- 
+#print(rahul) #it show error
+print('rahul')# its result is rahul 
+print('7'+'56')#ye string ka result hoga 756 
+print(7+5)#ye number ka result hoga 12
+#aur agar koi word lekhe quotes mai aur word ya words lekhne ke baad , lagaye aur aur word ya words lekhe to wo ek line mai commas ke sath print hoga  ex:-
+print("rahul's, son") #result  rahul's son
+print("rahul's","son")#to apne app bech mai space bana dega aur result aayega rahul's,   son
+
+#python mai repl ka full form hota hain 'read evalute print loop' aur ye python code to turant test karta hain
+#terminal mai python lekhne se repl open hota hain window mai aur mac mai python 3 lekhne se 
+#add karne ke liye'+' ex:
+print(5+6) #result=11
+#substract karne ke liye'-'
+print(5-3) #result=2
+#multiply karne ke liye'*'
+print(6*3) #result =18
+#divide karne ke liye'/'
+print(7/2) #result=3.5
+print('4th day complete')
