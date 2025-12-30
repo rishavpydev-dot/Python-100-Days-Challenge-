c792457377bb341dd37871ed21f3cc72d1d86b86
rishavpydev-dot <rishavpy.dev@gmail.com>
2025-12-30 10:03:40 +0530

    AddAdd Day 3: Modules and Pip notes

---
+++ day3_modules.py
@@ -0,0 +1,9 @@
+# Day 3: Modules and Pip
+# Aaj maine seekha ki modules do tarah ke hote hain: Built-in aur External.
+# External modules ko 'pip install' se install karte hain.
+
+import pandas # Yeh ek external module hai
+import os     # Yeh ek built-in module hai
+
+print("Day 3 Task Completed!")
+
